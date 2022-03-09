348185d126d3471fcfeb910ec2ae83e4c8c7d62f
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-03-08 21:25:16 -0300

    fixed local fetching

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,24 @@ function App() {
 	
 	useEffect(() => {
 		const fetchData = async () => {
-			const response = axios.get("/db/data.json");
-			const data = (await response).data;
+			try {
+				const response = axios.get("/db/data.json");
+				const data = (await response).data;
 
-			setCardData(data);
+				setCardData(data);
+			}	catch (err) {
+				// Ideally, we should make a useState to setFetchError,
+				// and conditionally render either the content of something related
+				// to the fetch error, like a component.
+				// For the sake of simplicity, I decided not to do it.  
+				console.log(err);
+			}
+
 		};
 
 		fetchData();
 
-		// Set Last timeframe data;
+		// Set Last timeframe data
 		const lastTimeframe =
 		timeframe === "daily" ? "Day" : timeframe === "weekly" ? "Week" : "Month";
 		setLastTimeframe(lastTimeframe);

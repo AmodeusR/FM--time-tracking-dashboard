4eca77b12a0c16bc82eb84029b1bfe5ed79ac306
Joel Kuzmarski <leoj3n@gmail.com>
2022-03-11 01:37:15 -0600

    Show difference of dev vs dist

--- src/styles/styled-components.js
+++ src/styles/styled-components.js
@@ -2,7 +2,9 @@ import styled from "styled-components";
 
 const CardArt = styled.div`
   border: 2px solid ${props => props.bgcolor};
-	background: ${props => `var(--theme-${props.iconRef});`}
+  &:after {
+    content: "${props => `${props.iconRef}`}";
+  }
 `;
 
 export default CardArt;
